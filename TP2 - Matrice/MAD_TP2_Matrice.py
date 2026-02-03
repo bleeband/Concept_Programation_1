39acdc2bc8675236edd41a1ced19d35d10386229
Marc-André Dufour <MAD@nottrue.test>
2026-02-03 13:42:08 -0500

    Ajout de la gestion des erreurs ; MAJ du README

--- TP2 - Matrice/MAD_TP2_Matrice.py
+++ TP2 - Matrice/MAD_TP2_Matrice.py
@@ -2,34 +2,36 @@
 
 import random
 
+under = "\033[4m"
+red = "\033[31m"
+end = "\033[0m"
+
 max_size = 7
-max_val = 55
+max_val = 99
 
 def user_input():
     while True:
         try:
+
             ligne = int(input("Veuillez entrer le nombre de ligne de votre matrice : "))
-            if ligne <= 0:
-                raise ValueError("La valeur doit être entre 1 et 7") 
-            if ligne > max_size:
-                raise ValueError(f"Valeur maximale autorisée : {max_size}")
-            
-            colonne = int(input(f"Veuillez entrer le nombre de colonne de votre matrice : "))
-            if colonne <= 0:
-                raise ValueError("La valeur doit être entre 1 et 7")
-            if colonne > max_size:
-                raise ValueError(f"Valeur maximale autorisée : {max_size}")
-            
-            val_max = int(input(f"Veuillez entrer la valeur maximale de votre matrice : "))
-            if val_max <= 0:
-                raise ValueError("La valeur doit être entre 1 et 7") 
-            if val_max > max_val:
+            if ligne <= 0 or ligne > max_size:
+                raise ValueError(f"La valeur doit être entre 1 et {max_size}") 
+
+            colonne = int(input("Veuillez entrer le nombre de colonne de votre matrice : "))    
+            if colonne <= 0 or colonne > max_size:
+                raise ValueError(f"La valeur doit être entre 1 et {max_size}")
+
+            val_max = int(input("Veuillez entrer la valeur maximale de votre matrice : "))
+            if val_max <= 0 or val_max > max_val:
                 raise ValueError(f"Valeur maximale autorisée : {max_val}") 
             
             return ligne, colonne, val_max
         
         except ValueError as ve:
-            print(f"Erreur : {ve} (ValueError1)")
+            print(f"{red}Erreur : {ve}{end}")
+
+        except Exception as e:
+            print(f"{red}Erreur inattendue : {e}{end}")
 
 def mat_generator(lignes, colonnes, max_val):
     return [[random.randint(1, max_val) for i in range(colonnes)] for j in range(lignes)]
@@ -42,10 +44,9 @@ def show_mat(matrice):
             print(f"{val:3}", end=" ")
         print()
 
-
 def executer():
+    pass
 
-    pass 
 
 # TEST
 nbr1, nbr2, nbr3 = user_input()
